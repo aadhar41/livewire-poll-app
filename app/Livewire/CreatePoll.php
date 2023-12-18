33ec196de9bfb848948d076aeab6d9efd20fd5fb
Aadhar gaur <aadhar41@gmail.com>
2023-12-18 15:08:37 +0530

    008 Creating a Poll

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
-    public $title = 'Post title...';
+    public $title;
     public $options = ['First'];
 
     public function render()
@@ -23,6 +24,20 @@ class CreatePoll extends Component
     {
         unset($this->options[$index]);
         $this->options = array_values($this->options);
+    }
+
+    public function createPoll()
+    {
+        $poll = Poll::create(
+            [
+                'title' => $this->title,
+            ]
+        );
+
+        foreach ($this->options as $optionName) {
+            $poll->options()->create(['name' => $optionName]);
+        }
+        $this->reset(['title', 'options']);
     }
 
     // public function mount(): Returntype
