b0f8a206a89a692a3ac8f45fa4b509b00c4c321a
Aadhar gaur <aadhar41@gmail.com>
2023-12-18 11:32:33 +0530

    006 Actions in Livewire

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,9 +7,19 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title = 'Post title...';
+    public $options = ['First'];
 
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    // public function mount(): Returntype
+    // {
+    // }
 }
