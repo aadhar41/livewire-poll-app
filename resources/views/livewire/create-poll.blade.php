80bc82410231e25e396cdff9515824af0ff79f23
Aadhar gaur <aadhar41@gmail.com>
2023-12-19 15:10:12 +0530

    010 Validation in Livewire

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -2,6 +2,9 @@
     <form wire:submit.prevent="createPoll" method="post">
         <label for="">Poll Title</label>
         <input type="text" wire:model.live="title" />
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         <div class="mb-4 mt-2">
             <button type="button" wire:click.prevent="addOption" class="btn btn-primarybtn-lg btn-block">
@@ -17,6 +20,9 @@
                         <input type="text" wire:model.live="options.{{ $index }}" />
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
